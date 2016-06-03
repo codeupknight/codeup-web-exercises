4f2932a9dcc271434660064906320f5dfb5f277c
Knight <beijingtexan@gmail.com>
2016-06-03 16:54:24 -0500

    exercises updated

--- public/array.js
+++ public/array.js
@@ -2,13 +2,14 @@
     "use strict";
 
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-    var names = ['John', 'TJ', 'Rob', 'Anthony', 'Eddie']
+    var names = ['John', 'TJ', 'Rob', 'Anthony', 'Eddie'];
     // TODO: Create a log statement that will log the number of elements in the names array.
-    console.log("There are " + names.length + " names.")
+    console.log("There are " + names.length + " names.");
     // TODO: Create log statements that will print each of the names array elements individually.
-    console.log(names[0] + " is a jerk.")
-    console.log(names[1] + " is full of himself.")
-    console.log(names[2] + " is an alcoholic and needs help.")
-    console.log(names[3] + " talks way too much.")
-    console.log(names[4] + " wrote this.")
+    for (var i =0; i < names.length; i++)
+    	console.log(names[i]);
+
+    names.forEach( function(element) {
+    	console.log("da name is " + element);
+    })
 })();
